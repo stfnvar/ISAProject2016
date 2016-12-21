f7891c2618d5dbcbbb3856aa46dbe9a783da4aa4
stfnvar <stefans94@hotmail.com>
2016-12-21 18:07:37 +0100

    .

---
+++ Restaurant/src/main/java/com/restaurant/repository/WaiterRepository.java
@@ -0,0 +1,10 @@
+package com.restaurant.repository;
+
+import com.restaurant.model.Waiter;
+import org.springframework.data.repository.Repository;
+
+public interface WaiterRepository extends Repository<Waiter, Long>  {
+
+	
+	
+}
